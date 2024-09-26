36bba3a6edf1040f5bca3d37a27e86a8f0d04961
Khusanboy Tursunov <tursunovkhusanboy@gmail.com>
2024-09-26 22:46:57 +0500

    update

--- lib/presentation/screens/delivery/deliveries_screen.dart
+++ lib/presentation/screens/delivery/deliveries_screen.dart
@@ -1,8 +1,6 @@
-import 'package:carpet_delivery/main.dart';
 import 'package:carpet_delivery/presentation/widgets/product_info_widget.dart';
 import 'package:carpet_delivery/utils/app_constants/app_colors.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class DeliveriesScreen extends StatelessWidget {
   const DeliveriesScreen({super.key});
